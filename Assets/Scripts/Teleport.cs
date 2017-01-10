b322022d0bfdd27045c1d5dd12093e4e6caef175
sachaandre <andresachaa@gmail.com>
2017-01-10 08:14:42 +0100

    Adding Teleporters ~ Scripting Teleport behaviour

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    [SerializeField]
+    private float posDep;
+    [SerializeField]
+    private float posArr;
+
+    private GameObject myPlayer;
+    private Transform myPos;
+
+
+    // Use this for initialization
+    void Start () {
+        myPlayer = GameObject.Find("player");
+        myPos = myPlayer.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Blowing")
+        {
+            Debug.Log("Hit");
+        }
+    }
+}
